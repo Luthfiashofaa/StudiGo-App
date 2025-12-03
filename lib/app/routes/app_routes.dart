17a22918d604f8d7090f12e203dc45a2b6dbff94
nabileka12345 <nabilekaputraaqilah@gmail.com>
2025-12-03 23:51:59 +0700

    update navigasi add schedule

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,16 +1,17 @@
 part of 'app_pages.dart';
-// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
 
 abstract class Routes {
   Routes._();
-  static const home = _Paths.home;
   static const splashscreen = _Paths.splashscreen;
   static const login = _Paths.login;
+  static const home = _Paths.home;
+  static const addschedule = _Paths.addschedule;
 }
 
 abstract class _Paths {
   _Paths._();
-  static const home = '/home';
   static const splashscreen = '/splashscreen';
   static const login = '/login';
+  static const home = '/home';
+  static const addschedule = '/add-schedule';
 }
