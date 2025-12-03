6ff5215787927536e112cc317c2d7eefcb51687b
nabileka12345 <nabilekaputraaqilah@gmail.com>
2025-12-03 23:51:59 +0700

    update navigasi add schedule

---
+++ lib/app/modules/bindings/schedule/add_schedule_binding.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class AddScheduleBinding extends Bindings {
+  @override
+  void dependencies() {
+    // Isi jika ada controller
+    // Get.lazyPut(() => AddScheduleController());
+  }
+}
